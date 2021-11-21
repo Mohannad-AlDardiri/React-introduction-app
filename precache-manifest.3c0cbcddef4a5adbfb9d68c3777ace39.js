7468c1c126fa86c48b69098929713250dda433d1
mohaned mohamed <mhnd62864@gmail.com>
2021-11-21 10:53:51 +0200

    Updates

---
+++ precache-manifest.3c0cbcddef4a5adbfb9d68c3777ace39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78e7544c53ad85d08bf1de0e357a6d4",
+    "url": "/React-introduction-app/index.html"
+  },
+  {
+    "revision": "b8023f2b79612b58974a",
+    "url": "/React-introduction-app/static/css/main.f2ec619d.chunk.css"
+  },
+  {
+    "revision": "25aef5db35b477605e2c",
+    "url": "/React-introduction-app/static/js/2.f2df14c7.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/React-introduction-app/static/js/2.f2df14c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8023f2b79612b58974a",
+    "url": "/React-introduction-app/static/js/main.65926103.chunk.js"
+  },
+  {
+    "revision": "11509a87fc76b7d13727",
+    "url": "/React-introduction-app/static/js/runtime-main.c83e956c.js"
+  }
+]);
